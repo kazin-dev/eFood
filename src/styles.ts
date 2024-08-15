6fde1e93a5d1d93bf7ee583fab33046fb138d295
kauã gomes <kgs26121967@gmail.com>
2024-08-15 15:32:16 -0300

    setup criado

---
+++ src/styles.ts
@@ -0,0 +1,11 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalCss = createGlobalStyle`
+  *{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    list-style: none;
+    font-family: Roboto, sans-serif;
+  }
+`
